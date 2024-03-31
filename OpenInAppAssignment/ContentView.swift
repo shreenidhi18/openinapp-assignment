0c8528937c153b99f961bb09c2257f6279844200
Shreenidhi <shreenidhi.vm@gmail.com>
2024-03-31 12:55:41 +0530

    Initial Commit

---
+++ OpenInAppAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenInAppAssignment
+//
+//  Created by shreenidhi vm on 31/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenInAppAssignment/OpenInAppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenInAppAssignmentApp.swift
+//  OpenInAppAssignment
+//
+//  Created by shreenidhi vm on 31/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenInAppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
